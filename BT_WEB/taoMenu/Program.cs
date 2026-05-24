e5f3c7d6d8838879af9477d61f7fef6e5b3855e6
Logg <hoangdebongtoi24@gmail.com>
2026-05-24 08:13:26 +0700

    menu_hinhhoc

---
+++ BT_WEB/taoMenu/Program.cs
@@ -0,0 +1,65 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int choice = -1;
+
+        while (choice != 0)
+        {
+            // Hiển thị menu
+            Console.WriteLine("Menu");
+            Console.WriteLine("1. Draw the triangle");
+            Console.WriteLine("2. Draw the square");
+            Console.WriteLine("3. Draw the rectangle");
+            Console.WriteLine("0. Exit");
+            Console.Write("Enter your choice: ");
+
+            choice = Int32.Parse(Console.ReadLine());
+
+            // Xử lý lựa chọn
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("Draw the triangle");
+                    Console.WriteLine("******");
+                    Console.WriteLine("*****");
+                    Console.WriteLine("****");
+                    Console.WriteLine("***");
+                    Console.WriteLine("**");
+                    Console.WriteLine("*");
+                    break;
+
+                case 2:
+                    Console.WriteLine("Draw the square");
+                    Console.WriteLine("* * * * * *");
+                    Console.WriteLine("* * * * * *");
+                    Console.WriteLine("* * * * * *");
+                    Console.WriteLine("* * * * * *");
+                    Console.WriteLine("* * * * * *");
+                    Console.WriteLine("* * * * * *");
+                    break;
+
+                case 3:
+                    Console.WriteLine("Draw the rectangle");
+                    Console.WriteLine("* * * * * *");
+                    Console.WriteLine("* * * * * *");
+                    Console.WriteLine("* * * * * *");
+                    break;
+
+                case 0:
+                    Console.WriteLine("Exit!");
+                    break;
+
+                default:
+                    Console.WriteLine("No choice!");
+                    break;
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
