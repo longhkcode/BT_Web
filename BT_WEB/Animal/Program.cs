5f4ab7021d311cbfce99c5dc39a5a5a0bce60b31
Logg <hoangdebongtoi24@gmail.com>
2026-05-24 08:50:29 +0700

    animal

---
+++ BT_WEB/Animal/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace Animal;
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Cat cat = new Cat("20kg", "1.5", "kitty");
+        cat.PrintInfo();
+    }
+}
