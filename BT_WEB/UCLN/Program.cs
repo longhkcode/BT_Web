5792cdc18b1ed39d715e6707bf71152a9c3c46ef
Logg <hoangdebongtoi24@gmail.com>
2026-05-24 08:14:34 +0700

    ucln

---
+++ BT_WEB/UCLN/Program.cs
@@ -0,0 +1,21 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Nhap vao so thu nhat : ");
+        int a = int.Parse(Console.ReadLine());
+        Console.Write("Nhap vao so thu hai : ");
+        int b = int.Parse(Console.ReadLine());
+
+        int ucln = (int) GCD(a, b);
+        Console.WriteLine("GCD({0},{1}) = {2}",a,b,ucln);
+    }
+
+    public static long GCD(long a, long b)
+    {
+        if(b == 0) {
+            return a;
+        }
+        return GCD(b, a%b);
+    }
+}
