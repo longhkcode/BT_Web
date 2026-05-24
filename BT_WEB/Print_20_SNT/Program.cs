4689a2c39affa4d6293455d4867714754e6d6b77
Logg <hoangdebongtoi24@gmail.com>
2026-05-24 08:15:02 +0700

    20_snt

---
+++ BT_WEB/Print_20_SNT/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+public class Program
+{
+    public static void Main()
+    {
+        int count = 0;
+        int num = 2;
+        while (count <= 20)
+        {
+            if (checkSNT(num))
+            {
+                Console.Write(num + " ");
+                count++;
+            }
+
+            num++;
+        }
+    }
+
+    public static bool checkSNT(int n)
+    {
+        if (n <= 1) return false;
+        for (int i = 2; i <= Math.Sqrt(n); i++)
+        {
+            if (n % i == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
