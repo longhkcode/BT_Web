8c8c4878627f62a265229f8c62081e85c96a48dc
Logg <hoangdebongtoi24@gmail.com>
2026-05-24 08:55:19 +0700

    fan

---
+++ BT_WEB/Fan/Fan.cs
@@ -0,0 +1,60 @@
+﻿namespace Fan;
+
+using System;
+
+class Fan
+{
+    public const int SLOW = 1;
+    public const int MEDIUM = 2;
+    public const int FAST = 3;
+
+    private int speed = SLOW;
+    private bool on = false;
+    private double radius = 5;
+    private string color = "blue";
+
+    public int Speed
+    {
+        get { return speed; }
+        set { speed = value; }
+    }
+
+    public bool On
+    {
+        get { return on; }
+        set { on = value; }
+    }
+
+    public double Radius
+    {
+        get { return radius; }
+        set { radius = value; }
+    }
+
+    public string Color
+    {
+        get { return color; }
+        set { color = value; }
+    }
+
+    public Fan()
+    {
+    }
+
+    public override string ToString()
+    {
+        if (on)
+        {
+            return "Fan is on\n" +
+                   "Speed: " + speed +
+                   "\nColor: " + color +
+                   "\nRadius: " + radius;
+        }
+        else
+        {
+            return "Fan is off\n" +
+                   "Color: " + color +
+                   "\nRadius: " + radius;
+        }
+    }
+}
---
+++ BT_WEB/Fan/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace Fan;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Fan fan1 = new Fan();
+        fan1.Speed = Fan.FAST;
+        fan1.Radius = 10;
+        fan1.Color = "yellow";
+        fan1.On = true;
+
+        Fan fan2 = new Fan();
+        fan2.Speed = Fan.MEDIUM;
+        fan2.Radius = 5;
+        fan2.Color = "blue";
+        fan2.On = false;
+
+        Console.WriteLine("Fan 1:");
+        Console.WriteLine(fan1.ToString());
+
+        Console.WriteLine();
+
+        Console.WriteLine("Fan 2:");
+        Console.WriteLine(fan2.ToString());
+    }
+}
