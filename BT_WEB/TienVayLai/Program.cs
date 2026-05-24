f27c8b8447296b849194fbf9b5614166d3cef924
Logg <hoangdebongtoi24@gmail.com>
2026-05-24 08:01:43 +0700

    tienlai

---
+++ BT_WEB/TienVayLai/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+double money = 1.0;
+int month = 1;
+double intersetRate = 1.0;
+Console.WriteLine("Enter investment amount: ");
+money = Double.Parse(Console.ReadLine());
+Console.WriteLine("Enter number of months: ");
+month = Int32.Parse(Console.ReadLine());
+Console.WriteLine("Enter annual interest rate in percentage: ");
+intersetRate = Double.Parse(Console.ReadLine());
+double totalInterset = 0;
+for(int i = 0; i < month; i++)
+{
+    totalInterset += money * (intersetRate/100)/12; 
+}
+Console.WriteLine("Total of interset: " + totalInterset);
