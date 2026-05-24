0485852550d975d8e7ecc53c0cb1060cadea5e48
Logg <hoangdebongtoi24@gmail.com>
2026-05-24 09:14:52 +0700

    stop

---
+++ BT_WEB/StopWatch/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace StopWatch;
+class Program
+{
+    static void SelectionSort(int[] arr)
+    {
+        int n = arr.Length;
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            int minIndex = i;
+
+            for (int j = i + 1; j < n; j++)
+            {
+                if (arr[j] < arr[minIndex])
+                {
+                    minIndex = j;
+                }
+            }
+
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        int[] arr = new int[100000];
+        Random random = new Random();
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            arr[i] = random.Next(100000);
+        }
+
+        StopWatch stopWatch = new StopWatch();
+
+        stopWatch.Start();
+
+        SelectionSort(arr);
+
+        stopWatch.Stop();
+
+        Console.WriteLine("Thoi gian thuc thi: " + stopWatch.GetElapsedTime() + " milliseconds");
+    }
+}
---
+++ BT_WEB/StopWatch/StopWatch.cs
@@ -0,0 +1,39 @@
+﻿namespace StopWatch;
+
+using System;
+
+class StopWatch
+{
+    private DateTime startTime;
+    private DateTime endTime;
+
+    public DateTime StartTime
+    {
+        get { return startTime; }
+    }
+
+    public DateTime EndTime
+    {
+        get { return endTime; }
+    }
+
+    public StopWatch()
+    {
+        startTime = DateTime.Now;
+    }
+
+    public void Start()
+    {
+        startTime = DateTime.Now;
+    }
+
+    public void Stop()
+    {
+        endTime = DateTime.Now;
+    }
+
+    public double GetElapsedTime()
+    {
+        return (endTime - startTime).TotalMilliseconds;
+    }
+}
