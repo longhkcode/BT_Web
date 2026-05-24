c293eee5637c55664dd25fef620564c31a6d52dc
Logg <hoangdebongtoi24@gmail.com>
2026-05-24 08:10:39 +0700

    snt

---
+++ BT_WEB/SNTnhohon100/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+    int dem = 0;
+    Console.WriteLine("Danh sach so nguyen to :");
+    for (int i = 2; i <= 100; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            if (i % j == 0)
+            {
+                dem++;
+            }
+        }
+
+        if (dem == 2)
+        {
+            Console.WriteLine(i);
+        }
+        dem = 0;
+    }
