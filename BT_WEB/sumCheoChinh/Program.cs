739a9fec26f13b53ace5efc5b55f8526768f3f48
Logg <hoangdebongtoi24@gmail.com>
2026-05-24 08:33:18 +0700

    sum

---
+++ BT_WEB/sumCheoChinh/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Nhap kich thuoc ma tran vuong n = ");
+        int n = int.Parse(Console.ReadLine());
+        
+        double[,] matrix = new double[n, n];
+        Console.WriteLine("Nhap cac phan tu cua ma tran:");
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                Console.Write("matrix[" + i + "][" + j + "] = ");
+                matrix[i, j] = double.Parse(Console.ReadLine());
+            }
+        }
+        double sum = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            sum += matrix[i, i];
+        }
+        
+        Console.WriteLine("\nMa tran vua nhap:");
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                Console.Write(matrix[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine("\nTong cac phan tu tren duong cheo chinh = " + sum);
+    }
+}
